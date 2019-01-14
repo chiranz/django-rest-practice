668ea337f602bc80e450b91fd3f3c891e64a78e4
Chiranz <chiranpoudyal@gmail.com>
2019-01-14 09:52:39 +0530

    initial commit & Serialization API

---
+++ snippets/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import snippet_detail, snippet_list
+
+
+urlpatterns = [
+    path('snippets/', snippet_list),
+    path('snippets/<pk>/', snippet_detail),
+]
